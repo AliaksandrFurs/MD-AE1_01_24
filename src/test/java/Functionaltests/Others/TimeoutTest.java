c8e63426eaeabca138817c3962e8ae78cad38d1f
Alex Furs <aliaksandrfurs@gmail.com>
2024-04-01 18:20:27 +0200

    Added timeout class with data provider

---
+++ src/test/java/Functionaltests/Others/TimeoutTest.java
@@ -0,0 +1,9 @@
+package Functionaltests.Others;
+
+import Datafactory.TimeoutFactory;
+import com.epam.tat.module4.Timeout;
+
+public class TimeoutTest {
+
+    Timeout timeout = TimeoutFactory.getTimeout();
+}
