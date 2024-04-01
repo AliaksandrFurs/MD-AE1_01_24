f2b22eb5a40bc02df93c970eff94b7cad969bf1d
Alex Furs <aliaksandrfurs@gmail.com>
2024-04-01 18:34:52 +0200

    Added cosinus tests

--- src/test/java/Dataproviders/TrigonometricalDataProvider.java
+++ src/test/java/Dataproviders/TrigonometricalDataProvider.java
@@ -1,6 +1,15 @@
 package Dataproviders;
 
+import org.testng.annotations.DataProvider;
+
 public class TrigonometricalDataProvider {
 
-
+    @DataProvider(name = "cosbasic", parallel = false)
+    public  Object [][] getCosValues(){
+        return  new Object [][]{
+                {0, 1},
+                {90, 0},
+                {180, -1}
+        };
+    }
 }
--- src/test/java/Functionaltests/Trigonometrical/CosTest.java
+++ src/test/java/Functionaltests/Trigonometrical/CosTest.java
@@ -1,11 +1,21 @@
 package Functionaltests.Trigonometrical;
 
 import Datafactory.CalculatorFactory;
+import Dataproviders.TrigonometricalDataProvider;
 import com.epam.tat.module4.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CosTest {
 
     Calculator calculator = CalculatorFactory.getCalculator();
+
+    @Test(dataProvider = "cosbasic", dataProviderClass = TrigonometricalDataProvider.class,
+            description = "Cosinus basic test", groups = {"smoke", "trigonometrical"})
+    public void basicCosTest(double value, double result){
+
+        Assert.assertEquals(calculator.cos(value), result);
+    }
 
 
 }
